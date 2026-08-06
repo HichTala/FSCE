73fa181eca43ed35cc09a9f3a9521e94529b3881
HichTala <98521878+HichTala@users.noreply.github.com>
2026-08-06 13:32:40 +0200

    add: implement DatasetMapperHuggingFace for loading Hugging Face datasets

--- fsdet/data/dataset_mapper.py
+++ fsdet/data/dataset_mapper.py
@@ -234,8 +234,8 @@ class DatasetMapperHuggingFace(DatasetMapper):
     def __call__(self, dataset_dict):
         dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
         # USER: Write your own image loading if it's not from a file
-        sample = self.hf_dataset[dataset_dict["dataset_idx"]]
-        image = utils.read_image(dataset_dict["file_name"], format=self.img_format)
+        sample = self.hf_dataset[dataset_dict["image_id"]]
+        image = np.array(sample["image"].convert(self.img_format))
         utils.check_image_size(dataset_dict, image)
 
         if "annotations" not in dataset_dict:
